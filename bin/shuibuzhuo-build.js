a4cff01faa7e81256debf9a6a5e160e334558e73
zhuotuo <zhuotuo@leagsoft.com>
2023-12-04 20:22:37 +0800

    feat: 参数解析

--- bin/shuibuzhuo-build.js
+++ bin/shuibuzhuo-build.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+const { program } = require("commander");
 
-console.log("shuibuzhuo build!");
+program.option("--first").option("-s --seperator <char>");
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.seperator, limit));
