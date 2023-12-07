e5c779986e1da607c5b0857017f8e29fb39cc755
zhuotuo <zhuotuo@leagsoft.com>
2023-12-07 13:45:56 +0800

    feat: 注册命令“

--- bin/shuibuzhuo-build.js
+++ bin/shuibuzhuo-build.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
-const { Command } = require("commander");
+const { program } = require("commander");
 const pkg = require("../package.json");
 const checkNode = require("../lib/checkNode");
+const startServer = require("../lib/start/startServer");
+const build = require("../lib/build/build");
 
 const MIN_NODE_VERSION = "8.9.0";
 
@@ -12,6 +14,22 @@ const MIN_NODE_VERSION = "8.9.0";
         "Please upgrade your node version to v" + MIN_NODE_VERSION
       );
     }
+
+    program.version(pkg.version);
+
+    program
+      .command("start")
+      .description("start server by shuibuzhuo-build")
+      .allowUnknownOption()
+      .action(startServer);
+
+    program
+      .command("build")
+      .description("build project by shuibuzhuo-build")
+      .allowUnknownOption()
+      .action(build);
+
+    program.parse();
   } catch (error) {
     console.log(error.message);
   }
