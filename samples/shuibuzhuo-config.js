40d18c6544614b6559af4f6ccef04740c3e04cf1
zhuotuo <zhuotuo@leagsoft.com>
2023-12-13 15:32:51 +0800

    feat: 配置文件修改重启服务优化

--- samples/shuibuzhuo-config.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-
-const entry = "src/index.js";
-module.exports = {
-  entry: path.isAbsolute(entry) ? entry : path.resolve(entry),
-  plugins: ["shuibuzhuo-build-test"],
-};
--- samples/shuibuzhuo-config.mjs
+++
@@ -1,7 +0,0 @@
-import path from "path";
-
-const entry = "src/index.js";
-module.exports = {
-  entry: path.isAbsolute(entry) ? entry : path.resolve(entry),
-  plugins: ["shuibuzhuo-build-test"],
-};
