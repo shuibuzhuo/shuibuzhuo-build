3944c7712a687d09fd1930803c2d96c149404921
zhuotuo <zhuotuo@leagsoft.com>
2023-12-21 09:16:57 +0800

    feat: 处理 html 模板

--- samples/plugins/shuibuzhuo-build-plugin.js
+++ samples/plugins/shuibuzhuo-build-plugin.js
@@ -1,11 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = function (api, options) {
   const dir = process.cwd();
 
-  // api
-  //   .getWebpackConfig()
-  //   .entry("login")
-  //   .add(path.resolve(dir, "./src/login.js"))
-  //   .end();
+  api.getWebpackConfig().plugin('HtmlWebpackPlugin2').use(HtmlWebpackPlugin, [{
+    filename: 'index2.html',
+    template: path.resolve(dir, './public/index2.html'),
+    chunks: ['index']
+  }]) 
 };
